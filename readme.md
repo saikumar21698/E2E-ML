38ab1b637cd5c203065cc078c71d30be2967948a
saikumar21698 <saikumar21698@gmail.com>
2024-05-27 11:11:55 +0530

    package setup.py

---
+++ readme.md
@@ -0,0 +1,8 @@
+Agenda
+
+1. New environment (github)
+2. setup.py
+3. requirement.txt
+
+src folder and create a package
+
---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+#src to act a package, so we need to create __init__py, when setup.py is running in howmanny folder it has __init__py so it build 
+
